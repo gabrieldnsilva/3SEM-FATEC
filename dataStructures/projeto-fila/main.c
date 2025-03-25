242cb82ceeebd6889f95354ab1b5bda1697a10af
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-03-25 13:09:28 -0300

    feat: main.c has adapted to work with new files senha.c/h

--- dataStructures/projeto-fila/main.c
+++ dataStructures/projeto-fila/main.c
@@ -1,46 +1,76 @@
 #include <stdio.h>
 #include "FilaSimples.h"
+#include "senha.h"
 
 // Constantes
 enum
 {
     OP_NAO_SELECIONADA = 0,
-    OP_ADICIONAR,
+    OP_GERAR_SENHA,
     OP_ATENDER,
     OP_SAIR
 };
 
 // Protótipos
 int menu();
-void fsImprimir();
 
 int main(void)
 {
     int opcao = OP_NAO_SELECIONADA;
-    int numero = 0;
+    Senha senha;
+    int tipoEscolhido;
+    int prioritario;
+
+    srand(time(NULL));
 
     while (opcao != OP_SAIR)
     {
         opcao = menu();
         switch (opcao)
         {
-        case OP_ADICIONAR:
+        case OP_GERAR_SENHA:
 
-            scanf("%d", &numero);
-            if (!fsAdicionar(numero))
+            printf("Escolha o tipo de atendimento:\n");
+            printf("1 - Clínico Geral (%s)\n", TIPO_CLINICO_GERAL);
+            printf("2 - Pediatria (%s)\n", TIPO_PEDIATRIA);
+            printf("3 - Preferencial (%s)\n", TIPO_PREFERENCIAL);
+            scanf("%d", &tipoEscolhido);
+
+            printf("É prioritário? (1 - Sim, 0 - Não): ");
+            scanf("%d", &prioritario);
+
+            if (tipoEscolhido >= 1 && tipoEscolhido <= NUM_TIPOS)
             {
-                printf("Fila cheia!\n");
+                senha = gerarSenha(contadores[tipoEscolhido - 1].tipo, prioritario);
+                if (!fsAdicionar(senha))
+                {
+                    printf("Fila cheia!\n");
+                }
+                else
+                {
+                    printf("Senha gerada: ");
+                    imprimirSenha(senha);
+                    fsImprimir();
+                }
+            }
+            else
+            {
+                printf("Tipo de atendimento inválido!\n");
             }
             break;
         case OP_ATENDER:
-            if (fsRetirar(&numero))
+            if (fsRetirar(&senha))
             {
-                printf("%d retirado da fila.\n", numero);
+                printf("Chamando senha: ");
+                imprimirSenha(senha);
             }
             else
             {
                 printf("Fila vazia!\n");
             }
+
+            fsImprimir();
+
             break;
         case OP_SAIR:
             break;
@@ -54,28 +84,11 @@ int main(void)
 int menu()
 {
     int op = OP_NAO_SELECIONADA;
-    printf("Menu\n");
-    printf("%d - Retirar Senha\n", OP_ADICIONAR);
+    printf("==== SISTEMA DE SENHAS - CLÍNICA MÉDICA ====\n");
+    printf("%d - Retirar Senha\n", OP_GERAR_SENHA);
     printf("%d - Atender\n", OP_ATENDER);
     printf("%d - Sair\n", OP_SAIR);
     printf("Digite a sua opcao: ");
     scanf("%d", &op);
     return op;
-}
-
-void fsImprimir()
-{
-    if (fsEstaVazia())
-    {
-        printf("Fila vazia!\n");
-    }
-    else
-    {
-        printf("+---+-----+\n");
-        for (int i = fsInicio; i < fsFim; i++)
-        {
-            printf("| %1d | %3d |\n", i, fsFila[i]);
-            printf("+---+-----+\n");
-        }
-    }
 }

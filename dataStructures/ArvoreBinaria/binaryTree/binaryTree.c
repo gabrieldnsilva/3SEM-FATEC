8d1159d05ceac9d60b79e3fbcf36e18686f7926e
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-05-14 07:48:02 -0300

    feat: add function calculateHeight

--- dataStructures/ArvoreBinaria/binaryTree/binaryTree.c
+++ dataStructures/ArvoreBinaria/binaryTree/binaryTree.c
@@ -135,3 +135,16 @@ struct Node *removeNode(int value, struct Node *root)
 
     return root;
 }
+
+int calculateHeight(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int leftHeight = calculateHeight(root->left);
+    int rightHeight = calculateHeight(root->right);
+
+    return (leftHeight > rightHeight) ? (leftHeight + 1) : (rightHeight + 1);
+}
